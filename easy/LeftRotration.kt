afaf95fdc04ff5073c52e1a7b858529c3e5d473f
sung-eun <sungeunj0@naver.com>
2022-02-04 16:06:38 +0900

    📝 Arrays: Left Rotation
    https://www.hackerrank.com/challenges/ctci-array-left-rotation/problem
    
    시간복잡도 O(n)
    공간복잡도 O(n^2)

---
+++ easy/LeftRotration.kt
@@ -0,0 +1,53 @@
+import java.io.*
+import java.math.*
+import java.security.*
+import java.text.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.function.*
+import java.util.regex.*
+import java.util.stream.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.jvm.*
+import kotlin.jvm.functions.*
+import kotlin.jvm.internal.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+
+/*
+ * Complete the 'rotLeft' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY a
+ *  2. INTEGER d
+ */
+
+fun rotLeft(a: Array<Int>, d: Int): Array<Int> {
+    val result = Array<Int>(a.size){-1}
+    a.forEachIndexed { i, num ->
+        var newIndex = i - d
+        if (newIndex < 0) {
+            newIndex += a.size
+        }
+        result[newIndex] = num
+    }
+    return result
+}
+
+fun main(args: Array<String>) {
+    val first_multiple_input = readLine()!!.trimEnd().split(" ")
+
+    val n = first_multiple_input[0].toInt()
+
+    val d = first_multiple_input[1].toInt()
+
+    val a = readLine()!!.trimEnd().split(" ").map{ it.toInt() }.toTypedArray()
+
+    val result = rotLeft(a, d)
+
+    println(result.joinToString(" "))
+}
